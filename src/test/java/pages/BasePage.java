3c6dd650cce613b3f9ab0beed2a4dd2fb760a122
ns-maud96 <ns.maud96@gmail.com>
2022-10-20 19:53:18 +0300

    First start

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+
+public class BasePage {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public static final String BASE_URL = "https://my.monkkee.com/#/";
+
+    BasePage(WebDriver driver) {
+
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize(); //открывает браузер в полном окне
+    }
+}
+
