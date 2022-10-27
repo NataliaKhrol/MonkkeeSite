e60bf97feca758dcf99e84f24175fde3c62492da
NatalyaKhrol <tashapas@yahoo.com>
2022-10-27 14:39:32 +0300

    New Tests were added

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,12 +1,18 @@
 package pages;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class HomePage extends BasePage {
 
     private By SEARCH_FIELD = By.id("appendedInputButton");
     private By SEARCH_BUTTON = By.cssSelector("[title=\"Search\"]");
+    private By MANAGE_TAG = By.xpath("//a[contains(text(),'Manage tags')]");
+    private By DELETE_TAG = By.cssSelector("[class=\"icon-trash icon-white\"]");
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -20,4 +26,19 @@ public class HomePage extends BasePage {
     public boolean isFound() {
         return driver.findElement(By.cssSelector("[class=\"search-explanation ng-binding\"]")).isDisplayed();
     }
-}
+
+
+    public void manageTags() {
+        driver.findElement(MANAGE_TAG).click();
+        driver.findElement(DELETE_TAG).click();
+    }
+
+    public String allertMessage() {
+      return driver.switchTo().alert().getText();
+    }
+
+       // Alert alert = getWebDriver().switchTo().alert();
+      //  String alertText = alert.getText();
+      //  System.out.println(alertText);
+    }
+
--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class HomeTest extends BaseTest {
@@ -11,6 +12,15 @@ public class HomeTest extends BaseTest {
         loginPage.openPage();
         loginPage.login(user, password);
         homePage.search("liebe");
-        assertTrue(homePage.isFound(), "ups");
+        assertTrue(homePage.isFound(), "The search has failed");
+    }
+
+    @Test
+    public void Tags() {
+        loginPage.openPage();
+        loginPage.login(user, password);
+        homePage.manageTags();
+        assertEquals(homePage.allertMessage(),"Do you really want to delete the tag? All entries related to the tag remain unchanged.");
+
     }
 }
